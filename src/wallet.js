2083b75b54d6a358077adbce17b067016ee6bcb9
Matt Bell <mappum@gmail.com>
2016-04-27 14:28:17 -0700

    Added wallet initialization (key generation/derivation, sync state)

--- src/wallet.js
+++ src/wallet.js
@@ -3,9 +3,11 @@ var EventEmitter = require('events')
 var async = require('async')
 var bitcoinjs = require('bitcoinjs-lib')
 var HDNode = bitcoinjs.HDNode
+var Script = bitcoinjs.script
 var inherits = require('inherits')
 var sublevel = require('level-sublevel')
 var to = require('flush-write-stream').obj
+var pkg = require('../package.json')
 
 function assertValidParams (params) {
   if (params == null) {
@@ -51,10 +53,16 @@ function Wallet (params, db, opts) {
   this.meta = this.db.sublevel('m', { valueEncoding: 'json' })
   this.unspent = this.db.sublevel('u')
   this.transactions = this.db.sublevel('t')
+  this.keys = this.db.sublevel('k')
+  this.blocks = this.db.sublevel('b')
   opts = opts || {}
+  this.lookAhead = opts.lookAhead || 100
 
   this._loadMeta(opts, (err) => {
     if (err) return this._error(err)
+    this.pubKey = this.key.getPublicKeyBuffer()
+    this.deriveKey = this.key.deriveHardened(0)
+    this.ready = true
     this.emit('ready')
   })
 }
@@ -62,6 +70,11 @@ inherits(Wallet, EventEmitter)
 
 Wallet.prototype._error = function (err) {
   this.emit('error', err)
+}
+
+Wallet.prototype.onceReady = function (cb) {
+  if (this.ready) return cb()
+  this.once('ready', cb)
 }
 
 Wallet.prototype.createWriteStream = function (opts) {
@@ -76,14 +89,13 @@ Wallet.prototype._processBlock = function (block, cb) {
 
 Wallet.prototype._loadMeta = function (opts, cb) {
   if (this.ready) return cb(new Error('Cannot call "_loadMeta" after ready'))
-  async.map([ 'sync', 'info', 'key' ],
-  this.meta.get.bind(this.meta), (err, res) => {
+  async.map([ 'sync', 'info', 'key' ], this.meta.get.bind(this.meta),
+  (err, res) => {
     if (err && !err.notFound) return cb(err)
     if (err && err.notFound) return this._initializeMeta(opts, cb)
     this.sync = res[0]
     this.info = res[1]
     this.key = HDNode.fromSeedBuffer(new Buffer(res[2].seed, 'base64'))
-    this.pubKey = this.key.getPublicKeyBuffer()
     cb(null)
   })
 }
@@ -100,22 +112,63 @@ Wallet.prototype._initializeMeta = function (opts, cb) {
     })
   }, (err) => {
     if (err) return cb(err)
-    this.sync = { height: 0, hash: null }
+    this.sync = {
+      height: 0,
+      hash: null,
+      keys: {
+        derived: 0,
+        seen: 0
+      }
+    }
     this.info = {
-      date: opts.date != null ? opts.date : Date.now()
+      date: opts.date != null ? opts.date : Date.now(),
+      version: pkg.version
     }
     crypto.randomBytes(64, (err, seed) => {
       if (err) return cb(err)
       this.key = HDNode.fromSeedBuffer(seed, this.params)
-      this.pubKey = this.key.getPublicKeyBuffer()
       this.meta.batch([
         { type: 'put', key: 'sync', value: this.sync },
         { type: 'put', key: 'info', value: this.info },
         { type: 'put', key: 'key', value: {
-          pubKey: this.pubKey.toString('base64'),
+          pubKey: this.key.getPublicKeyBuffer().toString('base64'),
           seed: seed.toString('base64')
         } }
       ], cb)
+    })
+  })
+}
+
+Wallet.prototype._derive = function (i, cb) {
+  return this.deriveKey.derive(i)
+}
+
+Wallet.prototype._deriveAll = function (cb) {
+  var keyIndex = this.sync.keys.seen + this.lookAhead
+  var derived = []
+  for (var i = 0; i < keyIndex; i++) {
+    derived.push(this._derive(i))
+  }
+  this.sync.keys.derived = keyIndex
+  this.meta.put('sync', this.sync, (err) => {
+    if (err) return cb(err)
+    cb(null, derived)
+  })
+}
+
+Wallet.prototype._keyElements = function (key) {
+  return [
+    key.getPublicKeyBuffer(),
+    key.getIdentifier()
+  ]
+}
+
+Wallet.prototype.filterElements = function (cb) {
+  this.onceReady(() => {
+    this._deriveAll((err, keys) => {
+      if (err) return cb(err)
+      var elements = keys.map(this._keyElements.bind(this))
+      cb(null, [].concat.apply([], elements))
     })
   })
 }
