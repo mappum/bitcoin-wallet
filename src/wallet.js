286de39c6cf0cfd0008059a0ab91fdc22daefaba
Matt Bell <mappum@gmail.com>
2016-05-10 01:59:03 -0700

    Added write stream, key helper methods

--- src/wallet.js
+++ src/wallet.js
@@ -1,12 +1,20 @@
+'use strict'
+
 var crypto = require('crypto')
 var EventEmitter = require('events')
 var async = require('async')
 var bitcoinjs = require('bitcoinjs-lib')
+var hash160 = bitcoinjs.crypto.hash160
 var HDNode = bitcoinjs.HDNode
 var Script = bitcoinjs.script
+var Address = bitcoinjs.address
+var debug = require('debug')('bitcoin-wallet')
 var inherits = require('inherits')
+var pumpify = require('pumpify').obj
 var sublevel = require('level-sublevel')
+var transaction = require('level-transactions')
 var to = require('flush-write-stream').obj
+var createFilterStream = require('./filterStream.js')
 var pkg = require('../package.json')
 
 function assertValidParams (params) {
@@ -50,20 +58,23 @@ function Wallet (params, db, opts) {
 
   this.params = params
   this.db = sublevel(db)
+  // TODO: create wrappers for these stores to make them smarter (rather than always using raw put/get)
   this.meta = this.db.sublevel('m', { valueEncoding: 'json' })
-  this.unspent = this.db.sublevel('u')
-  this.transactions = this.db.sublevel('t')
+  this.unspent = this.db.sublevel('u', { valueEncoding: 'json' })
+  this.transactions = this.db.sublevel('t', { valueEncoding: 'binary' })
   this.keys = this.db.sublevel('k')
-  this.blocks = this.db.sublevel('b')
+  this.blocks = this.db.sublevel('b', { valueEncoding: 'json' })
   opts = opts || {}
   this.lookAhead = opts.lookAhead || 100
 
   this._loadMeta(opts, (err) => {
     if (err) return this._error(err)
-    this.pubKey = this.key.getPublicKeyBuffer()
-    this.deriveKey = this.key.deriveHardened(0)
-    this.ready = true
-    this.emit('ready')
+    this.deriveKey = this.key.deriveHardened(0).neutered()
+    this._updateKeys((err) => {
+      if (err) return this._error(err)
+      this.ready = true
+      this.emit('ready')
+    })
   })
 }
 inherits(Wallet, EventEmitter)
@@ -77,14 +88,78 @@ Wallet.prototype.onceReady = function (cb) {
   this.once('ready', cb)
 }
 
-Wallet.prototype.createWriteStream = function (opts) {
-  return to((block, enc, cb) => this._processBlock(block, cb))
+Wallet.prototype.getAddress = function (key) {
+  var output = this._receiveScript(key.getPublicKeyBuffer())
+  return Address.toBase58Check(hash160(output), this.params.scriptHash)
 }
 
-Wallet.prototype._processBlock = function (block, cb) {
-  async.each(block.transactions, (tx, cb) => {
+Wallet.prototype.createAddress = function (cb) {
+  this.createKey((err, key) => {
+    if (err) return cb(err)
+    cb(null, this.getAddress(key))
+  })
+}
 
-  }, cb)
+Wallet.prototype.createKey = function (cb) {
+  var meta = transaction(this.meta, { valueEncoding: 'json' })
+  var key = this._derive(this.sync.keys.used)
+  this.sync.keys.used++
+  meta.put('sync', this.sync)
+  meta.commit((err) => {
+    if (err) return cb(err)
+    cb(null, key)
+  })
+}
+
+Wallet.prototype.createWriteStream = function () {
+  var filterStream = createFilterStream(this.keys)
+  var processStream = to((block, enc, cb) => this.processBlock(block, cb))
+  var writeStream = pumpify(filterStream, processStream)
+  writeStream.on('error', this._error.bind(this))
+  return writeStream
+}
+
+Wallet.prototype.processBlock = function (data, cb) {
+  var db = transaction(this.db, { valueEncoding: 'json' })
+
+  this.sync.height = data.block.height
+  this.sync.hash = data.block.header.getHash().toString('base64')
+
+  if (data.relevant.length === 0) {
+    db.put('sync', this.sync, { prefix: this.meta })
+    db.commit(cb)
+    this.emit('sync', this.sync)
+    return
+  }
+
+  var txHashes = []
+  var highestKey = this.sync.keys.used
+  for (let tx of data.relevant) {
+    var hash = tx.tx.getHash().toString('base64')
+    txHashes.push(hash)
+    // TODO: decide: should we be storing whole transaction or only relevant ins/outs?
+    db.put(hash, tx.tx.toBuffer(), { prefix: this.transactions })
+
+    for (let input of tx.relevant.ins) {
+      db.del(`${input.hash.toString('base64')}:${input.index}`, { prefix: this.unspent })
+    }
+
+    for (let output of tx.relevant.outs) {
+      db.put(`${hash}:${output.outpoint.index}`, output, { prefix: this.unspent })
+      highestKey = Math.max(highestKey, output.key.index)
+    }
+  }
+
+  db.put(data.block.getHash().toString('base64'), txHashes, { prefix: this.blocks })
+  // TODO: save in binary
+
+  this.sync.keys.used = highestKey + 1
+  this._updateKeys(db, (err, keys) => {
+    if (err) return cb(err)
+
+    db.put('sync', this.sync, { prefix: this.meta })
+    db.commit(cb)
+  })
 }
 
 Wallet.prototype._loadMeta = function (opts, cb) {
@@ -117,7 +192,7 @@ Wallet.prototype._initializeMeta = function (opts, cb) {
       hash: null,
       keys: {
         derived: 0,
-        seen: 0
+        used: 0
       }
     }
     this.info = {
@@ -131,7 +206,6 @@ Wallet.prototype._initializeMeta = function (opts, cb) {
         { type: 'put', key: 'sync', value: this.sync },
         { type: 'put', key: 'info', value: this.info },
         { type: 'put', key: 'key', value: {
-          pubKey: this.key.getPublicKeyBuffer().toString('base64'),
           seed: seed.toString('base64')
         } }
       ], cb)
@@ -139,37 +213,79 @@ Wallet.prototype._initializeMeta = function (opts, cb) {
   })
 }
 
-Wallet.prototype._derive = function (i, cb) {
-  return this.deriveKey.derive(i)
+Wallet.prototype._derive = function (i, hardened) {
+  var key = this.deriveKey
+  return hardened ? key.deriveHardened(i) : key.derive(i)
 }
 
-Wallet.prototype._deriveAll = function (cb) {
-  var keyIndex = this.sync.keys.seen + this.lookAhead
+// returns derived keys from index 'from' to 'used'+'lookAhead'
+Wallet.prototype._deriveRange = function (from) {
+  debug(`_deriveRange, from=${from}`)
+  var keyIndex = this.sync.keys.used + this.lookAhead
   var derived = []
-  for (var i = 0; i < keyIndex; i++) {
+  for (var i = from; i < keyIndex; i++) {
     derived.push(this._derive(i))
   }
-  this.sync.keys.derived = keyIndex
-  this.meta.put('sync', this.sync, (err) => {
-    if (err) return cb(err)
-    cb(null, derived)
-  })
+  debug(`_deriveRange done`)
+  return derived
+}
+
+// derives any keys up to used+lookAhead, saves to key store, emits 'filteradd'
+Wallet.prototype._updateKeys = function (db, cb) {
+  var shouldCommit = false
+  if (typeof db === 'function') {
+    cb = db
+    db = transaction(this.db)
+    shouldCommit = true
+  }
+
+  var from = this.sync.keys.derived
+  var to = this.sync.keys.used + this.lookAhead
+  if (from === to) return cb(null)
+  debug(`updating wallet keys. from=${from}, to=${to}`)
+
+  var keys = this._deriveRange(from)
+  var filterElements = []
+  for (let i = 0; i < keys.length; i++) {
+    let key = keys[i]
+    let index = from + i
+    let elements = this._keyElements(key)
+    for (let element of elements) {
+      db.put(element.toString('base64'), index, { prefix: this.keys })
+    }
+    filterElements.push(elements)
+  }
+
+  this.sync.keys.derived = to
+  db.put('sync', this.sync, { prefix: this.meta, valueEncoding: 'json' })
+
+  if (this.ready) {
+    this.emit('filteradd', [].concat(...filterElements))
+  }
+
+  if (shouldCommit) db.commit(cb)
+  else cb(null)
+}
+
+Wallet.prototype._redeemScript = function (pubkey) {
+  return Script.pubKeyOutput(pubkey)
+}
+
+Wallet.prototype._receiveScript = function (pubkey) {
+  var redeem = this._redeemScript(pubkey)
+  return Script.scriptHashOutput(hash160(redeem))
 }
 
 Wallet.prototype._keyElements = function (key) {
-  return [
-    key.getPublicKeyBuffer(),
-    key.getIdentifier()
-  ]
+  var pubkeyBuffer = key.getPublicKeyBuffer()
+  return [ pubkeyBuffer, this._receiveScript(pubkeyBuffer) ]
 }
 
 Wallet.prototype.filterElements = function (cb) {
   this.onceReady(() => {
-    this._deriveAll((err, keys) => {
-      if (err) return cb(err)
-      var elements = keys.map(this._keyElements.bind(this))
-      cb(null, [].concat.apply([], elements))
-    })
+    var keys = this._deriveRange(0)
+    var elements = keys.map(this._keyElements.bind(this))
+    cb(null, Array.prototype.concat.apply([], elements))
   })
 }
 
